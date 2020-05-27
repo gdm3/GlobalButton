299a893ae069baec6055a6afad0341128c12a079
Noel Bryan <Nbryan@nastudents.org>
2020-05-27 13:04:47 +0000

    Display

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template, Response, request, redirect, url_for
 app = Flask(__name__)
+data = 3
 @app.route('/')
 def redirct():
-  return render_template('index.html')
-@app.route('/background_process_test')
-def background_process_test():
-    print ("Hello")
+  return render_template('index.html', data=data)
+@app.route('/clicked', methods=['GET', 'POST']) 
+def clicked():
+    print ("Saving one")
     return ("nothing")
 app.run(host='0.0.0.0', port=8080)
