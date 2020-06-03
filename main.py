e8b253e2fb472abaf1372e71328798e7f6d483c4
Noel Bryan <Nbryan@nastudents.org>
2020-06-03 14:26:00 +0000

    idk tbh

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 from flask import Flask, render_template, Response, request, redirect, url_for
-txt = "PawskSmujwNjsohsrbhbad"
 from db import Check, Save, Send
-print(Check())
 app = Flask(__name__)
-data = Check()
 @app.route('/')
 def main():
-  return render_template('index.html', data=data)
+  return render_template('index.html', data=Check())
 @app.route('/about', methods=['GET', 'POST'])
 def about():
   return render_template('about.html')
@@ -25,7 +22,5 @@ def clicked():
     o = e + 1
     print(o)
     Save(o, e)
-    print("Saving one")
-    
     return render_template('index.html', data=o)
 app.run(host='0.0.0.0', port=8080)
